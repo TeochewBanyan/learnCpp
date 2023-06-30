7ded8d19eafd74bcc22ecd5c015cf5f7284ec869
Ti <872591581@qq.com>
2023-06-30 19:04:27 +0800

    fix bug: const int& func() cannot use as left-value

--- 0629/refAsRet.cpp
+++ 0629/refAsRet.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 
-const int &Add(int &a, const int &source);
+int &Add(int &a, const int &source);
 
-
-
-int main(){
-    int a=1, source=5;
+int main()
+{
+    int a = 1, source = 5;
     int four = 4;
 
     Add(a, source) = four;
-    std::cout<< "The result is " << a << std::endl;
+    std::cout << "The result is " << a << std::endl;
     return 0;
 }
 
-const int &Add(int &a, const int &source){
+const int &Add(int &a, const int &source)
+{
     a += source;
     return a;
 }
