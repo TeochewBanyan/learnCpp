a94a310a47a0eb4f2e53dadfd4da04aa4a2642ef
Ti <872591581@qq.com>
2023-06-30 18:59:04 +0800

    try reference var as return value, left value

---
+++ 0629/refAsRet.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+const int &Add(int &a, const int &source);
+
+
+
+int main(){
+    int a=1, source=5;
+    int four = 4;
+
+    Add(a, source) = four;
+    std::cout<< "The result is " << a << std::endl;
+    return 0;
+}
+
+const int &Add(int &a, const int &source){
+    a += source;
+    return a;
+}
